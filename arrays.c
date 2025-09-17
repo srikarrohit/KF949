7fed189ed073d6c6ac012b05b5eb1ff6518f27a2
unknown <srikarrohit@gmail.com>
2025-09-17 16:56:50 +0530

    insert and delete

--- arrays.c
+++ arrays.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
-void plusOne(int arr[],int size){
-    for(int i=0;i<size;i++){
+void plusOne(int arr[5]){
+    for(int i=0;i<5;i++){
         arr[i]+=1;
     }
 }
@@ -42,6 +42,44 @@ int recursiveBinSearch(int arr[],int left,int right,int key){
     }
 }
 //insertion and deletion into array
+void insertElement(int arr[],int key,int idx,int size,int newArray[]){
+    for(int i=0;i<idx;i++){
+        newArray[i]=arr[i];
+    }
+    newArray[idx]=key;
+    for(int i=idx+1;i<size+1;i++){
+        newArray[i]=arr[i-1];
+    }
+}
+int* insertElementUsingHeap(int arr[],int key,int idx,int size){
+    int *newArray=malloc(6*sizeof(int));
+    for(int i=0;i<idx;i++){
+        newArray[i]=arr[i];
+    }
+    newArray[idx]=key;
+    for(int i=idx+1;i<size+1;i++){
+        newArray[i]=arr[i-1];
+    }
+    return newArray;
+}
+void deleteElement(int arr[],int idx,int size,int newArray[]){
+    for(int i=0;i<idx;i++){
+        newArray[i]=arr[i];
+    }//1 2 3 4 5
+    for(int i=idx;i<size-1;i++){
+        newArray[i]=arr[i+1];
+    }
+}
+int* deleteElementUsingHeap(int arr[],int idx,int size){
+    int *newArray=malloc(4*sizeof(int));
+    for(int i=0;i<idx;i++){
+        newArray[i]=arr[i];
+    }
+    for(int i=idx;i<size-1;i++){
+        newArray[i]=arr[i+1];
+    }
+    return newArray;
+}
 int main(){
     int arr[5];
     arr[0]=1;//->2
@@ -51,11 +89,39 @@ int main(){
     arr[4]=5;
     int arr1[]={1,2,3,4,5};//one way of init
     int *arr2=malloc(10*sizeof(int));//dynamic array
-    plusOne(arr,5);//&arr[0]
+    //plusOne(arr,5);//&arr[0]
     for(int i=0;i<5;i++){
         printf("%d\t",arr[i]);
     }
     printf("\n%d\n",linearSearch(arr,4,5));
     printf("%d\n",iterativeBinSearch(arr,3,5));
     printf("%d\n",recursiveBinSearch(arr,0,4,5));
+    int array[]={9,3,4,5,6};
+    plusOne(array);
+    for(int i=0;i<5;i++){
+        printf("%d\t",array[i]);
+    }//will this array be updated
+    int newArray[6];
+    printf("\n");
+    insertElement(array,8,3,5,newArray);
+    for(int i=0;i<6;i++){
+        printf("%d\t",newArray[i]);
+    }
+    int *newArrayPtr=insertElementUsingHeap(array,8,3,5);
+    printf("\n");
+    for(int i=0;i<6;i++){
+        printf("%d\t",*(newArrayPtr+i));
+    }
+    int array1[]={1,2,3,4,5};
+    int newarray1[4];
+    deleteElement(array1,3,5,newarray1);
+    printf("\n");
+    for(int i=0;i<4;i++){
+        printf("%d\t",newarray1[i]);
+    }
+    int *newarray2=deleteElementUsingHeap(array1,3,5);
+    printf("\n");
+    for(int i=0;i<4;i++){
+        printf("%d\t",newarray2[i]);
+    }
 }
