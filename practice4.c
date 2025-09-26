6b1a4f3a54ef86c5c5ae2fb84d9e0295aae563b4
unknown <srikarrohit@gmail.com>
2025-09-26 16:54:13 +0530

    practice strings

--- practice4.c
+++ practice4.c
@@ -22,3 +22,35 @@ char* reverseStr(char* s, int k) {
     }
     return s;
 }
+char* toLowerCase(char* s) {
+    //what is the difference between character array and a string literal
+    //const keyword
+    //H--h
+    //A - 65
+    //a - 97
+    //A+32=a
+    for(int i=0;i<strlen(s);i++){
+        if(s[i]>='A' && s[i]<='Z'){
+            s[i]+=32;
+        }
+    }
+    return s;
+
+}
+bool isAnagram(char* s, char* t) {
+    if(strlen(s)!=strlen(t)){
+        return false;
+    }
+    int s_arr[26]={0};
+    int t_arr[26]={0};
+    for(int i=0;i<strlen(s);i++){
+        s_arr[tolower(s[i])-'a']++;
+        t_arr[tolower(t[i])-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(s_arr[i]!=t_arr[i]){
+            return false;
+        }
+    }
+    return true;
+}
